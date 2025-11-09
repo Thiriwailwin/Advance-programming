fbb0dcd22ddadbd4b464a7ff37340234062b865d
Thiriwailwin <thiriwailwin447@gmail.com>
2025-11-09 11:25:42 +0630

    addfiles

---
+++ binarysearch.py
@@ -0,0 +1,23 @@
+class BinarySearch:
+    @staticmethod
+    def search(arr, target):
+        left, right = 0, len(arr) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                return mid # Return the index of the target
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return -1 # Target not found
+
+# Example usage
+arr = [10, 20, 30, 40, 50]
+index = BinarySearch.search(arr, 30)
+
+if index != -1:
+    print(f"Element found at index {index}")
+else:
+    print("Element not found")
